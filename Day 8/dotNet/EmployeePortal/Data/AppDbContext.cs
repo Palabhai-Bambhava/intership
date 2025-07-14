de94e1f8b29705256b71f95316ee06128eaf55c8
Palabhai-Bambhava <bambhavapala@gmail.com>
2025-07-14 17:53:02 +0530

    Day 8 Task Done

---
+++ Day 8/dotNet/EmployeePortal/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using EmployeePortal.Models;
+using EmployeePortal.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeePortal.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>options) : base(options)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<AppUser> Users { get; set; }
+        //public object Users { get; internal set; }
+    }
+}
---
+++ Day 8/dotNet/EmployeePortal/Program.cs
@@ -0,0 +1,62 @@
+using EmployeePortal.Data;
+using EmployeePortal.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<EmployeeRepository>();
+builder.Services.AddScoped<IAuthRepository, AuthRepository>(); // ? Register AuthRepository
+
+
+
+// Configure JWT Authentication
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+
+builder.Services.AddAuthorization();
+
+var app = builder.Build();
+
+
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthentication(); // ? Add this BEFORE UseAuthorization
+app.UseAuthorization();
+
+app.UseCors(policy => policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+
+app.MapControllers();
+
+app.Run();
